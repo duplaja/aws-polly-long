a9197fdbef4076a09ad4d50d89013f7d0a077c9c
Dan D <dan.dulaney07@gmail.com>
2019-09-22 16:27:40 -0500

    Python3 Update, Removed / Updated Others
    * Added neural TTS engine support
    * Removed tagging (it was flawed)
    * Fixed some things for Python3
    * Changed up SSML for better pause control

--- audiobook.py
+++ audiobook.py
@@ -3,17 +3,14 @@ import subprocess
 import codecs
 
 #Enter / change below as needed
-inputfilename = "story.txt"
-outputname = "The-output-story-name.mp3"
-pollyvoice = "Brian"
+inputfilename = "input-text-file.txt" # Single blank line between paragraphs
+
+outputname = "your-output-file-name.mp3" #no spaces, will work on getting this straightened out
+
+pollyvoice = "Brian" #If you keep neural engine, make sure you pick a voice that supports it
 
 #Use country code first, no spaces or symbols. Ex) 12223334444. Leave as none if you don't want a text alert
 phone = "none"
-
-#Tagging Information, leave title as none to skip
-genre="none"
-title="none"
-author="none"
 
 #Begins the process
 f = codecs.open(inputfilename, encoding='utf-8')
@@ -22,40 +19,37 @@ cnt = 0
 
 for line in f:
     rendered = ''
-    line = line.replace('"', '\\"')
-    command = 'aws polly synthesize-speech --text-type ssml --output-format "mp3" --voice-id "'+pollyvoice+'" --text "{0}" {1}'
 
-    if '\r\n' == line:
+    #remove --engine neural if you don't want to use neural TTS (saves money, but more edges to tones)
+    command = 'aws polly synthesize-speech --text-type ssml --engine neural --output-format "mp3" --voice-id "'+pollyvoice+'" --text "{0}" {1}'
+
+    if '\n' == line:
         #A pause after a paragraph
-        rendered = '<speak><break time= "2s"/></speak>'
+        rendered = '<speak><break time="1200ms"/></speak>' #change this if you want less / more of a break between paragraphs
     else:
         #A pause after a sentence
-        rendered = '<speak><amazon:effect name=\\"drc\\">' + line.strip() + '<break time=\\"1s\\"/></amazon:effect></speak>'
+        linestrip = line.strip()
+        rendered = '<speak><amazon:effect name="drc">' + linestrip + '</amazon:effect></speak>'
+
+    rendered = rendered.replace('"', '\\"')
     
-    file_name = ' polly_out{0}.mp3'.format(u''.join(str(cnt)).encode('utf-8'))
+    file_name = ' polly_out{0}.mp3'.format(u''.join(str(cnt)).encode('utf-8').decode('utf-8'))
     cnt += 1
-    command = command.format(rendered.encode('utf-8'), file_name)
-    print command
+    command = command.format(rendered.encode('utf-8').decode('utf-8'), file_name)
+    print (command)
     subprocess.call(command, shell=True)
 
-    
 #Concatenates the audio files, one by one (to prevent an error with too many arguements)    
 num_add = 0
 while num_add < cnt:
-    execute_command = 'cat '+outputname+' polly_out' + str(num_add) + '.mp3 >> '+outputname
+    execute_command = 'cat polly_out' + str(num_add) + '.mp3 >> '+outputname
     subprocess.call(execute_command, shell=True)
     num_add += 1
 
 #Removes the tempoary files
 execute_command = 'rm polly_out*.mp3'
-print 'Removing temporary files: ' + execute_command
+print ('Removing temporary files: ' + execute_command)
 subprocess.call(execute_command, shell=True)
-
-#Sets appropriate audio tags
-if title != "none":
-    command = "mid3v2 -a \""+author+"\" -g \""+genre+"\" -t \""+title+"\" -A \""+title+" \""+outputname+"\""
-    subprocess.call(command, shell=True)
-    print 'Tags added'
 
 #Sends text alert that it is finished, if phone != "none"
 if phone != "none":
